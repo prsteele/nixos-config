a34f96d8b46acf92fa70fc6e4c8133d5cb6956d1
Patrick Steele <prsteele@proton.me>
2024-02-10 17:59:48 +0000

    correct networking configuration

--- machines/thinkpad-e14/default.nix
+++ machines/thinkpad-e14/default.nix
@@ -17,14 +17,13 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
+  # Use networkmanager, which supercedes wireless
+  networking.networkmanager.enable = true;
+  networking.wireless.enable = false;
+
+  networking.useDHCP = false;
   networking.interfaces.enp4s0.useDHCP = lib.mkDefault true;
   networking.interfaces.wlp5s0.useDHCP = lib.mkDefault true;
-  networking.wireless.enable = false;
 
   time.timeZone = "US/Eastern";
 
