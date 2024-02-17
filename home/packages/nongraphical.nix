a6d40138eb76efb7bba8cd31b54a4ffdc7b85da8
Patrick Steele <prsteele@proton.me>
2024-02-17 16:49:04 +0000

    feat: add python to the default shell

--- home/packages/nongraphical.nix
+++ home/packages/nongraphical.nix
@@ -3,15 +3,16 @@
   home.packages = with pkgs; [
     awscli2
     coreutils-full
+    curl
     emacs
     htop
     ispell
     jq
     nil
     nixpkgs-fmt
+    (python3.withPackages (ps: [ ps.pip ]))
     tree
     unzip
     zip
-    curl
   ];
 }
