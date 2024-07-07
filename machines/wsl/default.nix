c943aede87f2bcb524c6cebc9d1ee87af1fc663a
Patrick Steele <prsteele@proton.me>
2024-07-07 14:52:40 -0400

    feat: start the SSH agent on WSL

--- machines/wsl/default.nix
+++ machines/wsl/default.nix
@@ -29,6 +29,8 @@ let
     };
     home-manager.users.${user} = import ../../home;
 
+    programs.ssh.startAgent = true;
+
     # This value determines the NixOS release from which the default
     # settings for stateful data, like file locations and database versions
     # on your system were taken. It's perfectly fine and recommended to leave
