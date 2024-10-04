1950eeb5600a0a3209f8342f65fc750f96eb4dd4
Patrick Steele <prsteele@proton.me>
2024-10-04 00:20:46 -0400

    chore: simplify flake layout

--- flake.nix
+++ flake.nix
@@ -2,35 +2,56 @@
   description = "NixOS and nix-darwin configurations";
 
   inputs = {
+    # Nixpkgs
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs";
-    nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
-    home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
-      inputs.nixpkgs.follows = "nixpkgs";
+
+    # Mac support
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs-darwin";
     };
+    nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
     home-manager-darwin = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs-darwin";
     };
 
+    # WSL support
     nixos-wsl = {
       url = "github:nix-community/nixos-wsl";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # HM
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # NixOS generators
     nixos-generators = {
       url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-darwin = {
-      url = "github:LnL7/nix-darwin";
-      inputs.nixpkgs.follows = "nixpkgs-darwin";
+
+    # Known systems
+    systems = {
+      url = "github:nix-systems/default";
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, ... }@inputs:
+  outputs = { self, nixpkgs, nixpkgs-darwin, nixpkgs-unstable, nix-darwin, systems, ... }@inputs:
     let
-      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+      forAllSystems = nixpkgs.lib.genAttrs (import systems);
+
+      isDarwin = system: [ ] == builtins.match ".*-darwin" system;
+
+      mkPkgs = system:
+        if isDarwin system
+        then nixpkgs-darwin.legacyPackages.${system}
+        else nixpkgs.legacyPackages.${system};
+
     in
     {
       overlays = import ./overlays { inherit nixpkgs-unstable; };
@@ -46,10 +67,30 @@
         mbp = import ./machines/mbp inputs;
       };
 
-      packages = forAllSystems (system: {
-        aws-ami = self.nixosConfigurations.aws.config.formats.amazon;
-      });
+      packages = forAllSystems
+        (system:
+          let
+            pkgs = mkPkgs system;
+          in
+          {
+            rebuild = pkgs.writeShellApplication
+              {
+                name = "local-rebuild";
+                runtimeInputs = [ nix-darwin.packages.${system}.default ];
+                text =
+                  if isDarwin system
+                  then ''darwin-rebuild switch --flake "$@"''
+                  else ''nixos-rebuild switch --flake "$@"'';
+              };
 
-      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixpkgs-fmt);
+            aws-ami = self.nixosConfigurations.aws.config.formats.amazon;
+          });
+
+      formatter = forAllSystems (system:
+        let
+          pkgs = mkPkgs system;
+        in
+        pkgs.nixpkgs-fmt
+      );
     };
 }
